10055d58113202d2c6319573cc220edfe46a6313
ilyafefelov <ilyafefelov@gmail.com>
2017-09-30 15:53:52 +0300

    4

--- src/main/java/ua/danit/Square.java
+++ src/main/java/ua/danit/Square.java
@@ -1,6 +1,19 @@
 package ua.danit;
 
 public class Square {
+
+
+  private final int x;
+  private final int y;
+  private final int size;
+
+  public Square(int x, int y, int size)
+  {
+
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
   public String print()
   { return "<square></square>";}
 }
